e94a4295d0111e660d5a9326da0c831c97922a82
bogdanoancea <bogdan.oancea@gmail.com>
2019-04-04 17:34:09 +0300

    new class

---
+++ include/LocatableAgent.h
@@ -0,0 +1,19 @@
+/*
+ * LocatableAgent.h
+ *
+ *  Created on: Apr 4, 2019
+ *      Author: bogdan
+ */
+
+#ifndef LOCATABLEAGENT_H_
+#define LOCATABLEAGENT_H_
+
+#include <Agent.h>
+
+class LocatableAgent: public Agent {
+public:
+	LocatableAgent();
+	virtual ~LocatableAgent();
+};
+
+#endif /* LOCATABLEAGENT_H_ */
---
+++ src/LocatableAgent.cpp
@@ -0,0 +1,18 @@
+/*
+ * LocatableAgent.cpp
+ *
+ *  Created on: Apr 4, 2019
+ *      Author: bogdan
+ */
+
+#include "LocatableAgent.h"
+
+LocatableAgent::LocatableAgent() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LocatableAgent::~LocatableAgent() {
+	// TODO Auto-generated destructor stub
+}
+
