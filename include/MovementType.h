fb10f159475138fd07b88d93e70a06b8d85d622a
bogdanoancea <bogdan.oancea@gmail.com>
2019-05-16 11:31:03 +0300

    added MovementType

---
+++ include/MovementType.h
@@ -0,0 +1,21 @@
+/*
+ * Data simulator for mobile phone network events
+ *
+ * MovementType.h
+ *
+ *  Created on: May 15, 2019
+ *      Author: Bogdan Oancea
+ */
+
+#ifndef MOVEMENTTYPE_H_
+#define MOVEMENTTYPE_H_
+
+/*
+ *
+ */
+enum class MovementType {
+	RANDOM_WALK, RANDOM_WALK_WITH_DRIFT, UNKNOWN
+};
+
+
+#endif /* MOVEMENTTYPE_H_ */
