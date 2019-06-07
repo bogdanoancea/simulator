cde294cf2869a5bb516623904fea4f7619c60bc9
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2019-06-07 14:04:09 +0300

    new enum - prior type

---
+++ include/PriorType.h
@@ -0,0 +1,21 @@
+/*
+ * Data simulator for mobile phone network events
+ *
+ * MovementType.h
+ *
+ *  Created on: May 15, 2019
+ *      Author: Bogdan Oancea
+ */
+
+#ifndef PRIORTYPE_H_
+#define PRIORTYPE_H_
+
+/**
+ * An enum class that enumerates the types of the methods used to move the people on the map.
+ */
+enum class PriorType {
+	UNIFORM, REGISTER, NETWORK
+};
+
+
+#endif /* MOVEMENTTYPE_H_ */
