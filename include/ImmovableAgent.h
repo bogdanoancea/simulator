9b280a79831200513a80c264fe7bf82a48d98cad
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2019-09-04 11:44:08 +0300

    ImmovableAgent - comments and code cleaning.

--- include/ImmovableAgent.h
+++ include/ImmovableAgent.h
@@ -18,36 +18,36 @@ using namespace geos;
 using namespace geos::geom;
 
 /**
- * This is a class that represents an agent that can have a localion on map but it cannot move. The only subclass of it is Antenna.
+ * This is a class that represents an agent that can have a location on map but it cannot move. The only subclass of it is Antenna.
  */
 class ImmovableAgent: public LocatableAgent {
 public:
 
 	/**
-	 * Constructor of the class. Build an object of ImmovableAgent type with the parameters provided by the user.
-	 * @param m a pointer to a map object used in this simulation.
-	 * @param id the id of this object
-	 * @param initialPosition the initial location on map
-	 * @param clock a pointer to a Clock object used in this simulation
+	 * Constructor of the class. Build an ImmovableAgent object with the parameters provided by the user.
+	 * @param m a pointer to a Map object used in this simulation.
+	 * @param id the id of this object.
+	 * @param initialPosition the initial location on map.
+	 * @param clock a pointer to a Clock object used in this simulation.
 	 */
 	explicit ImmovableAgent(const Map* m, const unsigned long id, Point* initialPosition,
 			const Clock* clock);
 
 	/**
-	 * Destructor
+	 * Default destructor.
 	 */
 	virtual ~ImmovableAgent();
 
 	/**
-	 * Builds a string representation of this class
-	 * @return a string representation of this class
+	 * Builds a string representation of this class.
+	 * @return a string representation of this class. It is used to write details of the ImmovableAgent objects in a file or on console.
 	 */
 	const string toString() const override;
 
 
 	/**
-	 * Returns the name of this class
-	 * @return the name of this class
+	 * Returns the name of this class.
+	 * @return the name of this class.
 	 */
 	const string getName() const override;
 };
