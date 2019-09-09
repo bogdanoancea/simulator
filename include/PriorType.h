465210ea4f116bb815022a3b9c5045fc651cd929
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2019-09-09 14:28:29 +0300

    PriorType - comments

--- include/PriorType.h
+++ include/PriorType.h
@@ -11,7 +11,11 @@
 #define PRIORTYPE_H_
 
 /**
- * An enum class that enumerates the types of the methods used to move the people on the map.
+ * An enum class that enumerates the types of the prior used to computed the posterior localization probability.
+ * UNIFORM : the prior is an uniform probability, i.e. each object is equally located in each tile of the map.
+ * REGISTER: the prior probability is given by an administrative register.
+ * NETWORK: the prior probability is given by the mobile network - it is computed as the ratio between the signal quality
+ * given by Antenna a in tile t and the sum of the signal quality given by all antennas in all tiles of the grid.
  */
 enum class PriorType {
 	UNIFORM, REGISTER, NETWORK
