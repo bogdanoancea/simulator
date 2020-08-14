f9b1d063c7f771cda5993f9ee0e29554dcf5e7b9
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2020-08-14 09:47:35 +0300

    added Levy flights

---
+++ include/LevyFlightDisplacement.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2019  Bogdan Oancea
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version and under the EUPL free software license version 1.0 or later.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/> and 
+ * <https://ec.europa.eu/info/european-union-public-licence_en>
+ *
+ * LevyFlightDisplacement.h
+ *
+ *  Created on: Aug 13, 2020
+ *      Author: Bogdan.Oancea
+ */
+
+#ifndef INCLUDE_LEVYFLIGHTDISPLACEMENT_H_
+#define INCLUDE_LEVYFLIGHTDISPLACEMENT_H_
+
+#include <Displace.h>
+
+/**
+ * This class is part of the Strategy design pattern used to implement the displacement of persons on the map. It implements
+ * the Levy flights, overriding the generateNewLocation() method from its superclass, Displace.
+ */
+class LevyFlightDisplacement: public Displace {
+public:
+	/**
+	 * Constructor of the class. It only passes the arguments to the superclass, Displace.
+	 * @param map a pointer to the Map object of the simulation.
+	 * @param clk a pointer to the Clock object of the simulation.
+	 * @param speed the speed of displacement.
+	*/
+	LevyFlightDisplacement(Map* map, Clock* clk,  double speed);
+
+	/**
+	 * Implements the Levy flight behaviour. It takes a pointer to the current location, generates a uniformly distributed value
+	 * between 0 and 2 PI as the angle of displacement and computes the length of the step in this direction using the
+	 * speed and the time duration of a simulation step. If the new location is outside the map, it returns the current location, i.e. the object
+	 * will stay in the same location until the next simulation step.
+	 * @param p a pointer to the current location.
+	 * @return the new location
+	 */
+	virtual Point* generateNewLocation(Point* p) override;
+	virtual ~LevyFlightDisplacement();
+private:
+	virtual Point* computeNewLocation(Point* initLocation, double theta) override;
+	/**
+	 * Destructor. Does nothing.
+	*/
+
+};
+
+#endif /* INCLUDE_LEVYFLIGHTDISPLACEMENT_H_ */
+
+
---
+++ src/LevyFlightDisplacement.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2019  Bogdan Oancea
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version and under the EUPL free software license version 1.0 or later.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/> and 
+ * <https://ec.europa.eu/info/european-union-public-licence_en>
+ *
+ * LevyFlightDisplacement.cpp
+ *
+ *  Created on: Aug 13, 2020
+ *      Author: Bogdan.Oancea
+ */
+
+#include <LevyFlightDisplacement.h>
+#include <RandomNumberGenerator.h>
+#include <Utils.h>
+
+
+LevyFlightDisplacement::LevyFlightDisplacement(Map* map, Clock* clk,  double speed):
+	Displace(map, clk, speed) {
+}
+
+LevyFlightDisplacement::~LevyFlightDisplacement() {
+	// TODO Auto-generated destructor stub
+}
+
+Point* LevyFlightDisplacement::generateNewLocation(Point* initLocation) {
+	double theta = 0.0;
+	theta = RandomNumberGenerator::instance()->generateUniformDouble(0.0, 2 * utils::PI);
+	Point* pt = computeNewLocation(initLocation, theta);
+
+	Geometry* g = m_map->getBoundary();
+	if (!pt->within(g)) {
+		int k = 10;
+		while(k-- && !pt->within(g)) {
+			theta = RandomNumberGenerator::instance()->generateUniformDouble(0.0, 2 * utils::PI);
+			pt = computeNewLocation(initLocation, theta);
+		}
+		if(!k)
+			pt = initLocation;
+	}
+
+	return pt;
+}
+
+Point* LevyFlightDisplacement::computeNewLocation(Point* initLocation, double theta) {
+	double x = initLocation->getX();
+	double y = initLocation->getY();
+	double speed = RandomNumberGenerator::instance()->generateLevy(m_speed * 0.95, 10);
+	if (speed > m_speed *25)
+		speed = m_speed *25;
+	unsigned long delta_t = m_clock->getIncrement();
+	double newX = x + speed * cos(theta) * delta_t;
+	double newY = y + speed * sin(theta) * delta_t;
+	Coordinate c1 = Coordinate(newX, newY, 0);
+	Point* pt = m_map->getGlobalFactory()->createPoint(c1);
+	return pt;
+}
