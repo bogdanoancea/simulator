7c9331d4f23c3d001404e8de2a71bbb1238ae907
bogdanoancea <bogdan.oancea@gmail.com>
2020-10-20 14:10:15 +0300

    adde NetwrokType: 3G or 4G

---
+++ include/NetworkType.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2020  Bogdan Oancea
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version and under the EUPL free software license version 1.0 or later.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/> and
+ * <https://ec.europa.eu/info/european-union-public-licence_en>
+ *
+ * A data simulator for mobile phone network events
+ *
+ * EventType.h
+ *
+ *  Created on: Oct 14, 2020
+ *      Author: Bogdan Oancea
+ *      Email : bogdan.oancea@gmail.com
+ */
+
+#ifndef NETWORKTYPE_H_
+#define NETWORKTYPE_H_
+
+/**
+ * This class is an enumeration of network type currently supported:
+ * _3G
+ * _4G
+  */
+enum class NetworkType {
+	_3G, _4G
+};
+
+
+#endif /* NETWORKTYPE_H_ */
