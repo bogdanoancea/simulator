2b17df7c7fafebf17d5d22bf9a527e87724cca5c
bogdanoancea <bogdan.oancea@gmail.com>
2021-03-25 17:53:29 +0200

    working on home work scenario

---
+++ include/HomeWorkState.h
@@ -0,0 +1,22 @@
+/*
+ * WorkHomeState.h
+ *
+ *  Created on: Mar 25, 2021
+ *      Author: bogdan
+ */
+
+#ifndef INCLUDE_HOMEWORKSTATE_H_
+#define INCLUDE_HOMEWORKSTATE_H_
+
+/**
+ * This class is an enumeration of states of a person in a home-work simulation scenario:
+ * STAY_HOME
+ * GO_WORK
+ * STAY_WORK
+ * GO_HOME
+  */
+enum class HomeWorkState {
+	STAY_HOME, GO_WORK, STAY_WORK, GO_HOME
+};
+
+#endif /* INCLUDE_HOMEWORKSTATE_H_ */
