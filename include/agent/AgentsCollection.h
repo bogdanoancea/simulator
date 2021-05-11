9b6fd491e7e21acb8a167905fed1e9f295fc9a47
bogdanoancea <bogdan.oancea@gmail.com>
2021-05-11 18:15:38 +0300

    added comments

--- include/agent/AgentsCollection.h
+++ include/agent/AgentsCollection.h
@@ -36,9 +36,10 @@ using namespace std;
 typedef unordered_multimap<string, Agent*>::iterator um_iterator;
 
 /**
- * This is actually a container for all the agents used for simulation. An agent could be an object of one the the derived classes of Agent.
- * The Agents are kept in an unordered_multimap as pairs <string, Agent*> where the first element of the pair is
- * the name of the concrete agent (a person, a mobile device, an antenna, a mno, etc.)
+ * This is actually a container for all the agents used for simulation. An agent could
+ * be an object of one the the derived classes of \code{Agent}. The Agents are kept
+ * in an unordered_multimap as pairs of \code{ <string, Agent*>} where the first element of the pair is
+ * the name of the concrete agent (a person, a mobile device, an antenna, a mobile network operator, etc.)
  * and the second element is a pointer to the actual object (agent).
  */
 class AgentsCollection {
@@ -50,62 +51,66 @@ public:
 	AgentsCollection();
 
 	/**
-	 * Default destructor: it iterates through the collection of agents and frees the memory allocated for each agent in the collection.
+	 * Default destructor: it iterates through the collection of agents and frees the
+	 * memory allocated for each agent in the collection.
 	 */
 	virtual ~AgentsCollection();
 
 	/**
-	 * Adds a new Agent to the collection. For performance reasons the AgentsCollection class keep only a pointer to actual agents (objects).
-	 * @param a a pointer to the object (one of the derived classes of the Agent) to be added to the collection.
+	 * Adds a new \code{Agent} to the collection. For performance reasons the \code{AgentsCollection}
+	 * class keep only a pointer to actual agents (objects).
+	 * @param ag a pointer to the object (one of the derived classes of the \code{Agent}) to be added to the collection.
 	 */
-	void addAgent(Agent* a);
+	void addAgent(Agent* ag);
 
 	/**
 	 * Removes an object from the collection.
-	 * @param a a pointer to the object to be removed from the collection.
+	 * @param ag a pointer to the object to be removed from the collection.
 	 * @return a pointer to the object removed from the collection
 	 */
-	Agent* deleteAgent(Agent* a);
+	Agent* deleteAgent(Agent* ag);
 
 
 	/**
-	 * Returns a pointer to an agent from the collection. The agent/object is identified by its id.
-	 * @param id the id of the agent to be returned.
-	 * @return a pointer to the agent with the id equal to the parameter id. If there is no agent with the provided id,
-	 * this method returns nullptr.
+	 * Returns a pointer to an agent from the collection. The agent/object is identified by its
+	 * \code{id} which is unique throughout the collection.
+	 * @param id the \code{id} of the agent to be returned.
+	 * @return a pointer to the agent with the \code{id} equals to the parameter \param{id}.
+	 * If there is no agent with the provided \code{id}, this method returns \code{nullptr}.
 	 */
 	Agent* getAgent(const unsigned long id) const;
 
 	/**
-	 * This method is used to get a subset with a certain type of agents: persons, mobile phones etc.
+	 * This method is used to retrieve a subset of agents with a certain type: persons, mobile phones etc.
 	 * @param agentType is the name of the class of agents that the user wants to retrieve from the collection of all agents.
-	 * @return a std::pair of iterators of type unordered_multimap<string, Agent*>::iterator
+	 * @return a \code{std::pair} of iterators of type \code{unordered_multimap<string, Agent*>::iterator}
 	 * that can be used to iterate through to subset of the agents.
 	 */
 	pair<um_iterator, um_iterator> getAgentListByType(const string& agentType);
 
 	/**
-	 * Iterator to the past-the-end of the collection. It does not point to any agent, and thus shall not be dereferenced.
+	 * Iterator to the past-the-end object of the collection. It does not point to any agent,
+	 * and thus shall not be dereferenced.
 	 * @return an iterator referring to the past-the-end element in the agents container. If the container
-	 * is empty, this function returns the same as AgentsColletion::begin().
+	 * is empty, this function returns the same as \code{AgentsColletion::begin()}.
 	 */
 	um_iterator end();
 
 	/**
-	 * Iterator to the first agent of the container.
-	 * @return a random access iterator pointing to the first element (agent) in the container. If the container is empty, the returned
-	 * iterator value shall not be dereferenced.
+	 * Iterator to the first object of the container.
+	 * @return a random access iterator pointing to the first element (agent) in the container.
+	 * If the container is empty, the returned iterator value shall not be dereferenced.
 	 */
 	um_iterator begin();
 
 	/**
-	 * @return the number of elements in the container. This is the number of actual objects held in the container,
-	 * which is not necessarily equal to its storage capacity.
+	 * @return the number of elements in the container. This is the number of actual objects
+	 * held in the container,which is not necessarily equal to its storage capacity.
 	 */
 	unsigned long size();
 
 	/**
-	 * Print out all agents in the collection.
+	 * Prints out all agents in the collection calling \code{toString()} method of each agent.
 	 */
 	void printAgents();
 
