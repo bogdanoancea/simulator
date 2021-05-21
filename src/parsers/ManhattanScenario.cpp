7289f24846dd4b5a1b7a44c1d0bf5b04118c1cf1
bogdanoancea <bogdan.oancea@gmail.com>
2021-05-21 20:32:38 +0300

    added manhatan scenario

---
+++ src/parsers/ManhattanScenario.cpp
@@ -0,0 +1,18 @@
+/*
+ * MahattanScenario.cpp
+ *
+ *  Created on: May 20, 2021
+ *      Author: bogdan
+ */
+
+#include <parsers/ManhattanScenario.h>
+
+ManhattanScenario::ManhattanScenario() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ManhattanScenario::~ManhattanScenario() {
+	// TODO Auto-generated destructor stub
+}
+
