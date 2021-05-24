35f094ed68bb61b2f4afbd9ab72b916021ed40d2
bogdanoancea <bogdan.oancea@gmail.com>
2021-05-24 14:38:07 +0300

    comments

--- include/parsers/PersonConfiguration.h
+++ include/parsers/PersonConfiguration.h
@@ -29,19 +29,92 @@
 #include <agent/Person.h>
 #include <Distribution.h>
 
+/**
+ * This class encapsulates the parameters needed to generated a Person object. They are read from the persons configuration file.
+ * An object of this type is passed to the constructor of Person.
+ */
 class PersonConfiguration {
 public:
+
+	/**
+	 * Default constructor.
+	 */
 	PersonConfiguration();
+
+	/**
+	 * Default destructor.
+	 */
 	virtual ~PersonConfiguration();
+
+	/**
+	 * Returns the age of a person.
+	 * @return the age of a person.
+	 */
 	int getAge() const;
+
+	/**
+	 * Sets the age of a person.
+	 * @param age the age of a person.
+	 */
 	void setAge(int age);
+
+	/**
+	 * Returns the gender of a person: Person::Gender::MALE or Person::Gender::FEMALE.
+	 * @return the gender of a person: Person::Gender::MALE or Person::Gender::FEMALE.
+	 */
 	Person::Gender getGender() const;
+
+	/**
+	 * Sets the gender of a person (Person::Gender::MALE or Person::Gender::FEMALE).
+	 * @param gender the gender of a person (Person::Gender::MALE or Person::Gender::FEMALE).
+	 */
 	void setGender(Person::Gender gender);
+
+	/**
+	 * Returns a shared pointer to a Distribution object representing the probability distribution of the time interval between 2 consecutive stops of a person.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 * @return a shared pointer to a Distribution object representing the probability distribution of the time interval between 2 consecutive stops of a person.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 */
 	shared_ptr<Distribution> getIntervalBetweenStaysDistribution() const;
+
+	/**
+	 * Sets the Distribution object representing the probability distribution of the time
+	 * interval between 2 consecutive stops of a person.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 * @param intervalBetweenStaysDistribution * Sets the Distribution object representing the probability distribution of the time
+	 * interval between 2 consecutive stops of a person.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 *
+	 */
 	void setIntervalBetweenStaysDistribution(shared_ptr<Distribution> intervalBetweenStaysDistribution);
+
+	/**
+	 * Returns the speed of a person. This speed is generated according to a probability distribution specified in the persons configuration file.
+	 * @return the speed of a person. This speed is generated according to a probability distribution specified in the persons configuration file.
+	 */
 	double getSpeed() const;
+
+	/**
+	 * Sets the speed of a person. This speed is generated according to a probability distribution specified in the persons configuration file.
+	 * @param speed the speed of a person. This speed is generated according to a probability distribution specified in the persons configuration file.
+	 */
 	void setSpeed(double speed);
+
+	/**
+     * Returns a shared pointer to a Distribution object representing the probability distribution of the time interval a person stops in the same location.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 * @return a shared pointer to a Distribution object representing the probability distribution of the time interval a person stops in the same location.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 */
 	shared_ptr<Distribution> getTimeStayDistribution() const;
+
+	/**
+	 * Sets the Distribution object representing the probability distribution of the time intervals person stays in the same location.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 * @param timeStayDistribution the Distribution object representing the probability distribution of the time intervals person stays in the same location.
+	 * The parameters of this distribution are read from the persons configuration file.
+	 */
 	void setTimeStayDistribution(shared_ptr<Distribution> timeStayDistribution);
 
 private:
