1a50a9257b54f59916a69d7798f9eb5fe5793d9e
bogdanoancea <bogdan.oancea@gmail.com>
2021-05-31 16:23:08 +0300

    Manhattan mobility pattern

---
+++ include/Directions.h
@@ -0,0 +1,15 @@
+/*
+ * Directions.h
+ *
+ *  Created on: May 31, 2021
+ *      Author: bogdan
+ */
+
+#ifndef INCLUDE_DIRECTIONS_H_
+#define INCLUDE_DIRECTIONS_H_
+
+enum class Directions {
+	EAST, NORTH, WEST, SOUTH
+};
+
+#endif /* INCLUDE_DIRECTIONS_H_ */
