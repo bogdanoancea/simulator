d572a23df2ebadb29dc2c2e9f1f973a696a9e403
bogdanoancea <bogdan.oancea@gmail.com>
2021-06-01 12:27:34 +0300

    comments

--- include/ManhattanDisplacement.h
+++ include/ManhattanDisplacement.h
@@ -30,27 +30,39 @@
 
 /**
  * This class implements the Manhattan mobility pattern. It first computes the closest corner of the grid to the initial location
- * of the person. The first step mmoves the person between the  initial location on map and this corner.
- * Then it computes the distance a person can travel in a time step considering the travel speed. I
+ * of the person. The first step moves the person between the initial location on map and this corner.
+ * Then it computes the distance a person can travel in a time step considering the travel speed.
+ * At each time step, a new position is computed as follows. If the starting location is a corner of the Manhattan grid, it randomly select
+ * a direction to move. Then, while the distance to go is greater than zero, it checks if the Manhattan step length in the selected direction
+ * is less distance to go and if yes moves the person to the next corner, decreasing the distance to go by the step length. When the distance to go
+ * is less than the Manhattan step, it moves the person toward the next corner using the selected direction and it checks if it is close enough to this corner. "Close enough"
+ * means the the distance to go in one time step is less than the Manhattan step. If it is close enough, it moves the person directly to the corner, otherwise
+ * it leaves the person on the edge between two corners.
+ * If the starting location is on the edge between two corners, while the distance to go is greater than zero, it checks if the Manhattan step length in the selected direction
+ * is less distance to go and if yes moves the person to with a Manhattan step length along the selected direction, decreasing the distance to go by the step length. When the
+ * location of the person is close enough to a corner it moves the person directly to the corner. If the distance to go
+ * is less than the Manhattan step, it moves the person toward the next corner using the selected direction and it checks if it is close enough to this corner. If it is close enough, it moves the person directly to the corner, otherwise
+ * it leaves the person on the edge between two corners.
+ * If the new position is outside the map, the current location is kept unchanged, but a new direction for the next move is selected.
  */
 class ManhattanDisplacement: public Displace {
 public:
 	/**
-	 *
-	 * @param simconfig
-	 * @param speed
+	 * This is the constructor of the class. It stores internally the SimulationConfiguration object and the speed of the person.
+	 * @param simconfig a pointer to the SimulationConfiguration object
+	 * @param speed the speed of the person.
 	 */
 	ManhattanDisplacement(SimulationConfiguration* simconfig, double speed);
 
 	/**
-	 *
+	 * Default destructor.
 	 */
 	virtual ~ManhattanDisplacement();
 
 	/**
-	 *
-	 * @param initLocation
-	 * @return
+	 * Computes the new location after a time step. For details on how this location is computes see the explanation given for this class.
+	 * @param initLocation a pointer to a Point object which is the current location.
+	 * @return the new location after a time step.
 	 */
 	virtual Point* generateNewLocation(Point * initLocation) override;
 
